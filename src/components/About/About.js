274a20c70ccaddc4ac7f1ddf5bcd65815dedc504
chandanadc03 <chandanadc2003@gmail.com>
2023-02-03 15:25:53 +0530

    Corrected

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,14 +7,14 @@ function About() {
         <div className="about-nike">
           <h2 className="about-nike-text">OUR MISSION</h2>
           <p>
-            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE* IN THE WORLD"
+            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE IN THE WORLD"
           </p>
         </div>
       </div>
       <div className="about-nike-part">
         <h2 className="about-nike-heading">About Nike</h2>
         <p className='about-nike-para'>
-          Our mission is what drives us to do everything possible to expand
+          Our missions is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreaking sport
           innovations, by making our products more sustainably, by building a
           creative and diverse global team and by making a positive impact in
